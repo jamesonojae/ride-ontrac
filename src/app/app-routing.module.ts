db6c0681901ac070430c49e3fbc87e7824f67e82
jamesonojae <jamesonojae@gmail.com>
2020-09-16 11:08:29 +0100

    new commits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AboutComponent} from './components/about/about.component';
 
 
 const routes: Routes = [
@@ -10,8 +9,7 @@ const routes: Routes = [
   {
     path: 'index',
     loadChildren: () => import('./index/index.module').then(m => m.IndexModule)
-  },
-  {path: './about', component: AboutComponent}
+  }
 ];
 
 @NgModule({
