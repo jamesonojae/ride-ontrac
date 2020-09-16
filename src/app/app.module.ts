8ff35d0dd9a1515e1f864d4184bbe7a127ab1bb0
jamesonojae <jamesonojae@gmail.com>
2020-09-16 11:00:21 +0100

    more commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AboutComponent } from './components/about/about.component';
-import { OurservicesComponent } from './components/ourservices/ourservices.component';
-import { TeamComponent } from './components/team/team.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { FooterComponent } from './components/footer/footer.component';
+// import { AboutComponent } from './components/about/about.component';
+// import { OurservicesComponent } from './components/ourservices/ourservices.component';
+// import { TeamComponent } from './components/team/team.component';
+// import { ContactComponent } from './components/contact/contact.component';
+// import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    OurservicesComponent,
-    TeamComponent,
-    ContactComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
